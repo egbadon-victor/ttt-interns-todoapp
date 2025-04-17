6bad2fd8f06ae8d13adb28f04afc6f67e67c57ae
egbadon-victor <thethinkerx100@gmail.com>
2025-04-17 20:18:04 +0100

    complete implementation

--- index.js
+++ index.js
@@ -1,37 +1,61 @@
+const todoInputContainer = document.getElementById("todo-input-container");
 const todoInput = document.getElementById("todo-input");
 const todoInputButton = document.getElementById("todo-input-btn");
 const todoList = document.getElementById("todo-list");
+const todoListActions = document.querySelectorAll(".todo-list-action");
 
-const todos = [];
+const todos = [
+  {
+    value: "Todo 1",
+    completed: false,
+  },
+];
 
-const renderTodoListItem = (todoListItem) => `<li class="todo-item-container">
+const renderTodoListItem = (
+  todoListItem,
+  index
+) => `<li class="todo-item-container" data-index=${index}>
             <div class="todo-item">
-              <input type="checkbox" name="todo-item" />
-              <label for="todo-item">${todoListItem}</label>
+              <input type="checkbox" name="todo-item-${index}" ${
+  todoListItem.completed && "checked"
+} />
+              <span class="${todoListItem.completed && "completed"}">${
+  todoListItem.value
+}</span>
             </div>
             <div class="todo-item-actions">
-              <button class="todo-item-action-btn edit-btn">
+              <button class="todo-item-action-btn edit-btn" data-index=${index}>
                 <img src="./assets/icons/edit.svg" alt="edit" />
               </button>
-              <button class="todo-item-action-btn delete-btn">
+              <button class="todo-item-action-btn delete-btn data-index=${index}">
                 <img src="./assets/icons/delete.svg" alt="edit" />
               </button>
             </div>
           </li>`;
 
-const renderTodoList = () => {
+const renderTodoList = (status = "all") => {
+  let filteredTodos = [];
+
+  if (status === "completed")
+    filteredTodos = todos.filter((todo) => todo.completed);
+
+  if (status === "pending")
+    filteredTodos = todos.filter((todo) => !todo.completed);
+
+  if (status === "all") filteredTodos = todos;
+
   todoList.innerHTML = "";
-  todos.map((todo) => {
-    todoList.innerHTML += renderTodoListItem(todo.value);
+  filteredTodos.map((todo, index) => {
+    todoList.innerHTML += renderTodoListItem(todo, index);
   });
 
-  // For those more comfortable with for loops
+  //For those more comfortable with for loops
   //   for (let i = 0; i < todos.length; i++) {
-  //     todoList.innerHTML += renderTodoListItem(todos[i].value);
+  //     todoList.innerHTML += renderTodoListItem(todos[i].value, i);
   //   }
 };
 
-const addTodoItem = () => {
+const addTodoItem = (e) => {
   const inputValue = todoInput.value;
 
   if (inputValue !== "") {
@@ -45,5 +69,31 @@ const addTodoItem = () => {
 };
 
 todoInputButton.addEventListener("click", addTodoItem);
+
+todoList.addEventListener("click", (e) => {
+  const index = e.target.closest(".todo-item-container").dataset.index;
+  if (e.target.closest("input[type='checkbox']")) {
+    todos[index].completed = !todos[index].completed;
+  }
+  if (e.target.closest(".edit-btn")) {
+    const newValue = prompt("Edit your todo item", todos[index].value);
+    todos[index].value = newValue;
+  }
+  if (e.target.closest(".delete-btn")) {
+    todos.splice(index, 1);
+  }
+  renderTodoList();
+});
+
+Array.from(todoListActions).map((todoListAction) => {
+  todoListAction.addEventListener("click", (e) => {
+    Array.from(todoListActions).map((todoListAction) => {
+      todoListAction.classList.remove("todo-list-action-active");
+    });
+    todoListAction.classList.add("todo-list-action-active");
+    const filter = e.target.dataset.filter;
+    renderTodoList(filter);
+  });
+});
 
 renderTodoList();
